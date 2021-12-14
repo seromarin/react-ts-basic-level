3209a1afc40fc3f1c02d519ba92d1ce4fdd4f0d9
seromarin <rodriguez.marin.sebastian@gmail.com>
2021-12-13 19:04:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-basic
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-basic)
---
+++ index.tsx
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+import './style.css';
+
+import App from './app/App';
+
+render(<App />, document.getElementById('root'));
